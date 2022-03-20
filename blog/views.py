b50fdc371d91bc2dfdf48a44aac0e38b0c5070b4
lolasom <nds8195@naver.com>
2022-03-20 15:18:14 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,12 @@ def index(request):
         'blog/index.html',
         {'posts' : posts,}
     )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {'post' : post,}
+    )
