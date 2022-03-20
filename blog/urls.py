b7a2476e2173ac856db54456d215c8acd335c9ab
lolasom <nds8195@naver.com>
2022-03-20 20:33:38 +0900

    CBV로 블로그 포스트  상세페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
-    path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.PostDetail.as_view()),  # 자동으로 post_detail.html로 연결해줌
+    path('', views.PostList.as_view()),  # 자동으로 post_list.html로 연결해줌
+    # path('<int:pk>/', views.single_post_page),
     # path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -9,6 +9,12 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
     # template_name = 'blog/index.html'
+
+
+
+class PostDetail(DetailView):
+    model = Post
+
 
 
 
@@ -23,11 +29,11 @@ class PostList(ListView):
 #         {'posts' : posts,}
 #     )
 #
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {'post' : post,}
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {'post' : post,}
+#     )
